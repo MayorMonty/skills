910cc6d54c1febdd387296675ef5fb951f33f2b5
Brendan McGuire <artificalchamber@gmail.com>
2017-09-15 01:38:37 -0400

    Hello World

--- routes/home/index.js
+++ routes/home/index.js
@@ -6,7 +6,7 @@ import 'preact-material-components/Button/style.css';
 
 export default () => (
 	<div class={style.home}>
-		<h1>Home</h1>
+		<h1>Hello World</h1>
 		<p>This is the Home component.</p>
 		<Button ripple>Material Button</Button>
 	</div>
