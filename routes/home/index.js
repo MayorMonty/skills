162f26706f0f18a3b2f3affcf93b52a5f40062ad
Brendan McGuire <artificalchamber@gmail.com>
2017-09-15 02:10:48 -0400

    Add listing to home

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,13 +1,16 @@
-import { h } from 'preact';
-import style from './style';
+import { h, Component } from "preact";
+import style from "./style";
 
-import Button from 'preact-material-components/Button';
-import 'preact-material-components/Button/style.css';
+import SkillsList from "../../components/skillsList";
 
-export default () => (
-	<div class={style.home}>
-		<h1>Hello World</h1>
-		<p>This is the Home component.</p>
-		<Button ripple>Material Button</Button>
-	</div>
-);
+export default class Home extends Component {
+	render() {
+		return (
+			<div class={style.home}>
+				<h1>Home</h1>
+				<p>This is the Home component.</p>
+				<SkillsList type={2} program={"VRC"} />
+			</div>
+		);
+	}
+}
