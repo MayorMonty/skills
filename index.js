c7d354856107cba7de8053547c4af46eff7509ee
MayorMonty <artificalchamber@gmail.com>
2017-09-15 10:58:14 -0400

    Only register in production

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ import Header from './components/header';
 import Home from './routes/home';
 import Profile from './routes/profile';
 
+if(typeof window !== "undefined" && window.serviceWorker && process.env.NODE_ENV === "production") {
+	// @HACK: Preact-CLI forces you to use /sw.js, reregister the service worker
+	navigator.serviceWorker.getRegistrations().then(a => a.map( sw => sw.unregister() ));
+	console.log(`${__webpack_public_path__}sw.js`);
+	navigator.serviceWorker.register(`${__webpack_public_path__}sw.js`);
+}
+
 export default () => (
 	<div id="app">
 		<Header />
