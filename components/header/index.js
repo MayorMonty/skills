4cbce762e5ddddf6e4407cbb6ffb48bbfce3b8c6
MayorMonty <artificalchamber@gmail.com>
2017-09-17 18:17:42 -0400

    Make header material

--- components/header/index.js
+++ components/header/index.js
@@ -2,17 +2,33 @@ import { h, Component } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style';
 
+import Toolbar from 'preact-material-components/Toolbar';
+import 'preact-material-components/Toolbar/style.css';
+
+import Button from 'preact-material-components/Button';
+import 'preact-material-components/Button/style.css';
+
+
 export default class Header extends Component {
 	render() {
 		return (
-			<header class={style.header}>
-				<h1>Preact App</h1>
-				<nav>
-					<Link activeClassName="active" href="/">Home</Link>
-					<Link activeClassName="active" href="/profile">Me</Link>
-					<Link activeClassName="active" href="/profile/john">John</Link>
-				</nav>
-			</header>
+			<div>
+				<Toolbar className="toolbar" style={style.toolbar}>
+					<Toolbar.Row>
+					<Toolbar.Section align-start={true}>
+						<Toolbar.Icon menu={true}>menu</Toolbar.Icon>
+						<Toolbar.Title>
+						VEX Skills Helper
+						</Toolbar.Title>
+					</Toolbar.Section>
+					<Toolbar.Section align-end={true}>
+						<Button icon ripple compact>
+							<Toolbar.Icon>filter_list</Toolbar.Icon>
+						</Button>
+					</Toolbar.Section>
+					</Toolbar.Row>
+				</Toolbar>
+			</div>
 		);
 	}
 }
