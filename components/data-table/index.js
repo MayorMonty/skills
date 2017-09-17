367ec07b88d5401f5e0a70d1b6906d266909e555
MayorMonty <artificalchamber@gmail.com>
2017-09-17 18:17:32 -0400

    Start of Material Table Implementation

---
+++ components/data-table/index.js
@@ -0,0 +1,43 @@
+/**
+ * Adapted from mdl-data-table (Copyright Google)
+ */
+import { h, Component } from 'preact';
+import style from './style';
+
+import Checkbox from 'preact-material-components/Checkbox';
+import 'preact-material-components/Checkbox/style.css';
+
+
+export default class DataTable extends Component {
+	render({ headers, values }) {
+        console.log(style);
+		return (
+			<table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp" style={style["mdl-data-table"]}>
+                <thead>
+                    <tr>
+                    <th class="mdl-data-table__cell--non-numeric">Material</th>
+                    <th>Quantity</th>
+                    <th>Unit price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td class="mdl-data-table__cell--non-numeric">Acrylic (Transparent)</td>
+                    <td>25</td>
+                    <td>$2.90</td>
+                    </tr>
+                    <tr>
+                    <td class="mdl-data-table__cell--non-numeric">Plywood (Birch)</td>
+                    <td>50</td>
+                    <td>$1.25</td>
+                    </tr>
+                    <tr>
+                    <td class="mdl-data-table__cell--non-numeric">Laminate (Gold on Blue)</td>
+                    <td>10</td>
+                    <td>$2.35</td>
+                    </tr>
+                </tbody>
+                </table>
+		);
+	}
+}
