bb57d82bcc8019aeb0957b2e880357b824c14d9a
MayorMonty <artificalchamber@gmail.com>
2017-09-17 18:33:14 -0400

    Make Hack Command Actually work

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,8 @@
 preact build --template index.html
-
-sed -i -e 's/navigator.serviceWorker.register("/sw.js")/navigator.serviceWorker.register("/skills/sw.js")/g' build/
-
+find build/ -maxdepth 1 -name 'bundle.*.js' -print0 | xargs -0 -I {} -P 0 sed -i -e 's/\/sw\.js/\/skills\/sw\.js/g' {}
 git add build/
-
 today=`date +%Y-%m-%d.%H:%M:%S`
-
 git commit -m "${today} build"
-
 git push
-
 git subtree push --prefix build origin gh-pages
+echo "Done."
