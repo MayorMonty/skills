9f84818236f47aea91a42adbfcc43a1cc4698aa2
MayorMonty <artificalchamber@gmail.com>
2017-09-18 01:01:45 -0400

    Super Basic Table Implementation

--- components/data-table/index.js
+++ components/data-table/index.js
@@ -10,34 +10,31 @@ import 'preact-material-components/Checkbox/style.css';
 
 export default class DataTable extends Component {
 	render({ headers, values }) {
-        console.log(style);
 		return (
-			<table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp" style={style["mdl-data-table"]}>
+			<table class={style["mdl-data-table"]}>
                 <thead>
                     <tr>
-                    <th class="mdl-data-table__cell--non-numeric">Material</th>
-                    <th>Quantity</th>
-                    <th>Unit price</th>
+                        { Object.values(headers).map( prop => 
+                            <th class={style["mdl-data-table__cell--non-numeric"]}>{ prop }</th>
+                        ) }
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                    <td class="mdl-data-table__cell--non-numeric">Acrylic (Transparent)</td>
-                    <td>25</td>
-                    <td>$2.90</td>
-                    </tr>
-                    <tr>
-                    <td class="mdl-data-table__cell--non-numeric">Plywood (Birch)</td>
-                    <td>50</td>
-                    <td>$1.25</td>
-                    </tr>
-                    <tr>
-                    <td class="mdl-data-table__cell--non-numeric">Laminate (Gold on Blue)</td>
-                    <td>10</td>
-                    <td>$2.35</td>
-                    </tr>
+                    { values.map( run => 
+                        <tr key={`${run.sku}-${run.team}-${run.type}`}>
+                            { Object.keys(headers).map( prop => 
+                                <td class={style["mdl-data-table__cell--non-numeric"]}>
+                                    { [run, ...prop.split(".")].reduce((a,b) => a[b]) }
+                                </td> ) 
+                            }
+                        </tr>
+                    ) }
                 </tbody>
                 </table>
 		);
 	}
 }
+
+{/* <td class={style["mdl-data-table__cell--non-numeric"]}>
+                                { [run, ...prop.split(".")].reduce((a,b) => a[b]) }
+                            </td> */}
