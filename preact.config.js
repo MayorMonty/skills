a510fc2b181651470ce40aaa20e76f7bb47d3295
MayorMonty <artificalchamber@gmail.com>
2017-09-18 01:41:54 -0400

    Allow async await using babel-transform runtime

--- preact.config.js
+++ preact.config.js
@@ -2,5 +2,19 @@ export default function(config, env, helpers) {
     if (env.production) {
         config.output.publicPath = "/skills/"
     }
+
+    // Enable Async/Await
+    let babelLoader = helpers.getLoadersByName(config, "babel-loader")[0].rule.options.plugins;
+
+    babelLoader.push("transform-regenerator");
+    babelLoader.push([
+      "transform-runtime",
+      {
+        helpers: false,
+        polyfill: false,
+        regenerator: true
+      }
+    ]);
+
     return config;
 }
