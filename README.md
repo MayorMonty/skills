0d4ccaa761593dc1f3d26789692ab64af5f02099
MayorMonty <artificalchamber@gmail.com>
2017-09-18 01:42:39 -0400

    2017-09-18.01:42:39 build

--- README.md
+++
@@ -1,5 +0,0 @@
-# VEX Skills
-
-Explore skills data by region, program, grade, and a whole bunch of other factors
-
-https://mayormonty.github.io/skills/
--- deploy.sh
+++
@@ -1,8 +0,0 @@
-preact build --template index.html
-find build/ -maxdepth 1 -name 'bundle.*.js' -print0 | xargs -0 -I {} -P 0 sed -i -e 's/\/sw\.js/\/skills\/sw\.js/g' {}
-git add build/
-today=`date +%Y-%m-%d.%H:%M:%S`
-git commit -m "${today} build"
-git push
-git subtree push --prefix build origin gh-pages
-echo "Done."
