724d46890d5bc7fbfb28cb02cb46848bbd1f0dce
MayorMonty <artificalchamber@gmail.com>
2017-09-18 22:17:32 -0400

    Start of Dialog

---
+++ components/config-dialog/index.js
@@ -0,0 +1,7 @@
+import Dialog from 'preact-material-components/Dialog';
+import Button from 'preact-material-components/Button';
+import List from 'preact-material-components/List';
+import 'preact-material-components/List/style.css';
+import 'preact-material-components/Button/style.css';
+import 'preact-material-components/Dialog/style.css';
+
