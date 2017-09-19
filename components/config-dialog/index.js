b80b29448a4c889fa1a1142f2fc39ae92782456d
MayorMonty <artificalchamber@gmail.com>
2017-09-18 23:59:44 -0400

    Config Dialog Basics

--- components/config-dialog/index.js
+++ components/config-dialog/index.js
@@ -1,7 +1,27 @@
-import Dialog from 'preact-material-components/Dialog';
-import Button from 'preact-material-components/Button';
-import List from 'preact-material-components/List';
-import 'preact-material-components/List/style.css';
-import 'preact-material-components/Button/style.css';
-import 'preact-material-components/Dialog/style.css';
+import {h, Component} from 'preact';
+import Dialog from "preact-material-components/Dialog";
+import Button from "preact-material-components/Button";
+import List from "preact-material-components/List";
+import "preact-material-components/List/style.css";
+import "preact-material-components/Button/style.css";
+import "preact-material-components/Dialog/style.css";
 
+import store from "../../store/main"
+
+export default () => 
+    <Dialog ref={ref=>store.set("refs.config", ref)}>
+        <Dialog.Header>Scroll for me ;)</Dialog.Header>
+        <Dialog.Body scrollable={true}>
+        <List>
+            <List.Item>Item 1</List.Item>
+            <List.Item>Item 2</List.Item>
+            <List.Item>Item 3</List.Item>
+            <List.Item>Item 4</List.Item>
+            <List.Item>Item 5</List.Item>
+        </List>
+        </Dialog.Body>
+        <Dialog.Footer>
+        <Dialog.FooterButton cancel={true}>Decline</Dialog.FooterButton>
+        <Dialog.FooterButton accept={true}>Accept</Dialog.FooterButton>
+        </Dialog.Footer>
+    </Dialog>
