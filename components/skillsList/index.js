37384604bb0bbb3a2d917c546cdbb16d8a278b53
MayorMonty <artificalchamber@gmail.com>
2017-09-18 22:18:54 -0400

    Update list on store update

--- components/skillsList/index.js
+++ components/skillsList/index.js
@@ -8,6 +8,8 @@ import DataTable from '../data-table';
 import List from 'preact-material-components/List';
 import 'preact-material-components/List/style.css';
 
+import store from "../../store/main";
+
 export default class SkillsList extends Component {
 
     state = {
@@ -15,11 +17,23 @@ export default class SkillsList extends Component {
     }
 
     componentDidMount() {
-        getSkills(this.props.filters)
-            .then(list => this.setState({
-                list,
-                loaded: true
-            }))
+        this.getList();
+        store.on("update", () => {
+            this.getList();
+            this.setState({ loaded: false, list: [] });
+        });
+    }
+
+    componentWillRecieveProps() {
+        this.setState({ loaded: false });
+    }
+
+    getList() {
+        getSkills(store.get("filters"))
+        .then(list => this.setState({
+            list,
+            loaded: true
+        }))
     }
 
 	render({}, { loaded, list }) {
