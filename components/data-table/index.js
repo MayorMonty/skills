d7f90cc2e5952d2bc15c0ef11e960e6d48f26d5f
MayorMonty <artificalchamber@gmail.com>
2017-09-18 22:17:54 -0400

    Make statesless, use central dlv

--- components/data-table/index.js
+++ components/data-table/index.js
@@ -1,42 +1,39 @@
 /**
  * Adapted from mdl-data-table (Copyright Google)
  */
-import { h, Component } from 'preact';
+import { h } from 'preact';
 import style from './style';
+
+import { dlv } from "../../lib/util";
 
 import Checkbox from 'preact-material-components/Checkbox';
 import 'preact-material-components/Checkbox/style.css';
 
 
-export default class DataTable extends Component {
-	render({ headers, values }) {
-		return (
-			<table class={style["mdl-data-table"]}>
-                <thead>
-                    <tr>
-                        <th class={style["mdl-data-table--checkbox"]}>
-                            <Checkbox></Checkbox>
-                        </th>
-                        { Object.values(headers).map( prop => 
-                            <th class={style["mdl-data-table__cell--non-numeric"]}>{ prop }</th>
-                        ) }
-                    </tr>
-                </thead>
-                <tbody>
-                    { values.map( run => 
-                        <tr key={`${run.sku}-${run.team}-${run.type}`}>
-                            <td>
-                                <Checkbox></Checkbox>
-                            </td>
-                            { Object.keys(headers).map( prop => 
-                                <td class={style["mdl-data-table__cell--non-numeric"]}>
-                                    { [run, ...prop.split(".")].reduce((a,b) => a[b]) }
-                                </td> ) 
-                            }
-                        </tr>
-                    ) }
-                </tbody>
-                </table>
-		);
-	}
-}
+export default ({ headers, values }) =>
+    <table class={style["mdl-data-table"]}>
+        <thead>
+            <tr>
+                <th class={style["mdl-data-table--checkbox"]}>
+                    <Checkbox></Checkbox>
+                </th>
+                { Object.values(headers).map( prop => 
+                    <th class={style["mdl-data-table__cell--non-numeric"]}>{ prop }</th>
+                ) }
+            </tr>
+        </thead>
+        <tbody>
+            { values.map( run => 
+                <tr key={`${run.sku}-${run.team}-${run.type}`}>
+                    <td>
+                        <Checkbox></Checkbox>
+                    </td>
+                    { Object.keys(headers).map( prop => 
+                        <td class={style["mdl-data-table__cell--non-numeric"]}>
+                            { dlv(run, prop) }
+                        </td> ) 
+                    }
+                </tr>
+            ) }
+        </tbody>
+        </table>
